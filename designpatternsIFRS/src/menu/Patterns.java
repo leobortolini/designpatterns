7cea9ccf7f6fc036118aa937c1faa26ffc279a07
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:56:56 -0300

    Create método main e classe que chama os exemplos de patterns

---
+++ designpatternsIFRS/src/menu/Patterns.java
@@ -0,0 +1,52 @@
+package menu;
+
+public class Patterns {
+	
+	public static void abstractFactory() {
+		abstractfactory.Main.main(null);
+	}
+	
+	public static void bridge() {
+		bridge.Main.main(null);
+	}
+	
+	public static void builder() {
+		builder.Demo.main(null);
+	}
+	
+	public static void chainOfResponsability() {
+		chainofresponsability.exampleOneTest.main(null);
+	}
+	
+	public static void composite() {
+		composite.Main.main(null);
+	}
+	
+	public static void facade() {
+		facade.Main.main(null);
+	}
+	
+	public static void flyWeight() {
+		flyweight.CounterStrike.main(null);
+	}
+	
+	public static void mediator() {
+		mediator.Main.main(null);
+	}
+	
+	public static void memento() {
+		memento.Main.main(null);
+	}
+	
+	public static void prototype() {
+		prototype.Main.main(null);
+	}
+	
+	public static void proxy() {
+		proxy.Proxy.main(null);
+	}
+	
+	public static void visitor() {
+		visitor.Sistema.main(null);
+	}
+}
---
+++ designpatternsIFRS/src/menu/Principal.java
@@ -0,0 +1,8 @@
+package menu;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	}
+}
