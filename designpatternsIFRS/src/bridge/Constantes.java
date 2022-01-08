1d02f3f36716ec73563835ed4c4473329b6636ed
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 16:00:26 -0300

    Add comentario sobre bridge

---
+++ designpatternsIFRS/src/bridge/Constantes.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public final class Constantes {
+	public static final String about = "BRIDGE\n\n"
+			+ "O padrão Bridge é estrutural, se baseia na ideia de separar a "
+			+ "classe em duas partes: abstração e implementação. Assim, podemos "
+			+ "desenvolver ambas partes separadamente. No exemplo temos dois objetos"
+			+ "que possuem sua abstração (veículo), e sua implementação (Caminhao e Onibus)";
+}
