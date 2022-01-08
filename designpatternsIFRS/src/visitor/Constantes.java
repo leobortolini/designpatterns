8d31ef9f44d6a43bf7bdfe4eaf4450b6407d595e
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:43:33 -0300

    add comentario sobre visitor

---
+++ designpatternsIFRS/src/visitor/Constantes.java
@@ -0,0 +1,7 @@
+package visitor;
+
+public final class Constantes {
+	public static final String about = "VISITOR\n\n"
+			+ "Esse padrão permite separar o código de operação dos objetos do objeto em si, "
+			+ "movendo-os para outra classe.";
+}
