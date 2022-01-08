26a286043c8d8346fa491b5ce3a19718f622f2f9
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:13:19 -0300

    Códigos descomentados, para aumentar o exemplo

--- designpatternsIFRS/src/mediator/Main.java
+++ designpatternsIFRS/src/mediator/Main.java
@@ -7,21 +7,21 @@ public class Main {
 		Pista pista = new Pista(atcMediator);
 		
 		Voo voo1 = new Voo(atcMediator);
-		//Voo voo2 = new Voo(atcMediator);
+		Voo voo2 = new Voo(atcMediator);
 
 		
 		atcMediator.registrarVoo(voo1);
 		atcMediator.registrarPista(pista);
 		
 		voo1.prontoParaPousar();
-		//pista.pouso();
-		//voo1.pouso();
+		pista.pouso();
+		voo1.pouso();
 		
-		//voo2.pouso();
+		voo2.pouso();
 		
 		voo1.estacionado();
 		
-		//voo2.pouso();
+		voo2.pouso();
 		
 	}
 }
