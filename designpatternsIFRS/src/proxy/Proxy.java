3575661f64750916d3b4f23fd6fdf3028139e5b2
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 20:02:23 -0300

    add comentarios sobre a implementação do proxy

--- designpatternsIFRS/src/proxy/Proxy.java
+++ designpatternsIFRS/src/proxy/Proxy.java
@@ -4,6 +4,8 @@ package proxy;
 public class Proxy {
 
     public static void main(String[] args) {
+    	//chamando o construtor proxy de universidade, que verifica os parametros antes de instanciar uma Universidade
+    	//esse parametro, com base nas regras implementadas, nao permite a criação da universidade
         Universidade universidade = new UniversidadeProxy("errado");
         System.out.println("Login Incorreto");
         System.out.println(universidade.getNomeInstituicao());
@@ -11,6 +13,8 @@ public class Proxy {
         System.out.println("");
         System.out.println("----------------------------");
         System.out.println("");
+      //chamando o construtor proxy de universidade, que verifica os parametros antes de instanciar uma Universidade
+    	//esse parametro, com base nas regras implementadas, permite a criação da universidade
         universidade = new UniversidadeProxy("correto");
         System.out.println("Login válido");
         System.out.println(universidade.getNomeInstituicao());
