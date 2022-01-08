bffb8fc8e55eda41c69edd6502ff7f5294c5a070
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:04:47 -0300

    Add comentário sobre fly weight

---
+++ designpatternsIFRS/src/flyweight/Constantes.java
@@ -0,0 +1,10 @@
+package flyweight;
+
+public class Constantes {
+	public static final String about = "FLY WEIGHT\n\n"
+			+ "Esse design pattern é estrutural, possibilita o compartilhamento de atributos comuns"
+			+ "entre múltiplos objetos, ao invés de todos terem a mesma coisa instanciada diversas"
+			+ "vezes. No nosso exemplo temos os CounterTerrorist e Terrorists, que compartilham"
+			+ "as armas mas não as missões."; 
+
+}
