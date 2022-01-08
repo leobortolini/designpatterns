5297ef30afec5180e39b7b52ee3091ac680fd156
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 15:57:37 -0300

    Add comentario sobre abstract factory

---
+++ designpatternsIFRS/src/abstractfactory/Constantes.java
@@ -0,0 +1,12 @@
+package abstractfactory;
+
+public final class Constantes {
+	public static final String about = "Abstract Factory\n\n"
+			+ "O design pattern Abstract Factory é um padrão criacional que "
+			+ "permite a criação de objetos sem deixar explícito o nome de sua "
+			+ "classe. Um exemplo seria implementar um botão, que é diferente"
+			+ "dependendo do Sistema Operacional do usuário. Toda distinção de "
+			+ "implementação desses objetos pode se concentrar no pattern "
+			+ "Abstract Factory. No exemplo temos a criação de 4 tipos de "
+			+ "carros utilizando 2 factories e sem especificar o nome da classe";
+}
