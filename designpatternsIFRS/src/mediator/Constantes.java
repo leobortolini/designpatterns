52d059da6e2e8ed73497c8fc13dd85b12941eddc
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:12:57 -0300

    Add comentario sobre mediator

---
+++ designpatternsIFRS/src/mediator/Constantes.java
@@ -0,0 +1,10 @@
+package mediator;
+
+public final class Constantes {
+	public static final String about = "MEDIATOR\n\n"
+			+ "Esse padrão adiciona uma camada de comunicação entre"
+			+ "os objetos. No exemplo temos a comunicação entre dois Voo e"
+			+ "a Pista. Ambos Voos comunicam o mediator quando pretendem"
+			+ "pousar ou nao, e o mediator que gerencia a disponibilidade,"
+			+ "dependendo do seu uso";
+}
