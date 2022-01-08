26815842db20e6f69549d351f0f34ccb697ca74a
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 16:08:12 -0300

    Add comentário sobre builder

---
+++ designpatternsIFRS/src/builder/Constantes.java
@@ -0,0 +1,9 @@
+package builder;
+
+public final class Constantes {
+	public static final String about = "BUILDER\n\n" 
+			+ "O Builder é um padrão que possibilita criar objetos em etapas, "
+			+ "permitindo implementar versões diferentes de um objeto usando a mesma construção." 
+			+ "No exemplo, temos o objeto do tipo Diretor, que direciona a forma que os objetos que "
+			+ "implementam a interface Builder deve construir os objetos do tipo Car";
+}
