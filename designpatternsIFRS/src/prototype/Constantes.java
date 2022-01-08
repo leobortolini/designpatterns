e34a459e0544d12d41eb815d84cd845c0306c4b2
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:23:08 -0300

    Add comentario sobre prototype

---
+++ designpatternsIFRS/src/prototype/Constantes.java
@@ -0,0 +1,9 @@
+package prototype;
+
+public final class Constantes {
+	public static final String about = "PROTOTYPE\n\n"
+			+ "Esse padrão possibilita clonar objetos com baixo "
+			+ "acoplamento de classes. No exemplo, temos a clonagem"
+			+ "de objetos do tipo Person, através do método Clone, que"
+			+ "é a implementação da interface Cloneable";
+}
