74e74ae5cb610f68ae7a2a0c2c8d5402493d8ae4
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 16:56:53 -0300

    Add comentario sobre facade

---
+++ designpatternsIFRS/src/facade/Constantes.java
@@ -0,0 +1,8 @@
+package facade;
+
+public final class Constantes {
+	public static final String about = "FACADE\n\n"
+			+ "É um pattern usado para facilitar o uso de sistemas complexos, "
+			+ "fornecendo uma interface simplificada com apenas a parte que será usada pelo usuário/programador."
+			+ "No exemplo, com apenas um método executamos todos necessários para o Computador funcionar.";
+}
