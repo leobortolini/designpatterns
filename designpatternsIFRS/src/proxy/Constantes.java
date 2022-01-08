6e4e7036a1e72566d9f380743178b4563a214da5
Leonardo Bortolini <neijoleo@gmail.com>
2022-01-08 17:28:40 -0300

    add comentarios sobre proxy

---
+++ designpatternsIFRS/src/proxy/Constantes.java
@@ -0,0 +1,9 @@
+package proxy;
+
+public final class Constantes {
+	public static final String about = "PROXY\n\n"
+			+ "Esse design pattern possibilita adicionar controles de acesso "
+			+ "ao objeto original, por meio de outro objeto. No nosso exemplo, "
+			+ "a UniversidadeProxy verifica os dados inseridos antes de instanciar"
+			+ "um objeto do tipo Universidade";
+}
